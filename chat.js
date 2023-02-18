9743abecdf197cb123ba6d6b78a2ce4b67da8bdd
Marsell0 <m.marsox0d@mail.ru>
2023-02-19 03:23:43 +0500

    chat ui

--- chat.js
+++ chat.js
@@ -2,16 +2,29 @@ createFullChat();
 showChat();
 closeFullChat();
 
+document.getElementById('txtInput').addEventListener('input', ()=>{
+    document.getElementById('start').style.display = 'none'
+    document.getElementById('sendBtn').style.display = 'inline-flex'
+})
+document.getElementById('txtInput').addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+      sendMessage();
+    }
+  });
+
+const chatBody = document.querySelector(".chat-body");
 const URL = 'http://127.0.0.1:8000/send_voice';
-let div = document.createElement('div');
-div.id = 'msg';
+let img = document.createElement('img')
+img.setAttribute('src', 'src/mic.svg')
 let start = document.createElement('button');
 start.id = 'start';
-start.innerHTML = 'Start';
+start.appendChild(img)
+let img1 = document.createElement('img')
+img1.setAttribute('src', 'src/stop.svg')
 let stop = document.createElement('button');
 stop.id = 'stop';
-stop.innerHTML = 'Stop';
-document.getElementById('place_for_btns').appendChild(div);
+stop.appendChild(img1)
+// document.getElementById('place_for_btns').appendChild(div);
 document.getElementById('place_for_btns').appendChild(start);
 document.getElementById('place_for_btns').appendChild(stop);
 navigator.mediaDevices.getUserMedia({ audio: true})
@@ -42,13 +55,21 @@ navigator.mediaDevices.getUserMedia({ audio: true})
         });
     });
 
+document.getElementById('start').addEventListener('click', ()=>{
+    document.getElementById('start').style.display = 'none'
+    document.getElementById('stop').style.display = 'inline-flex'
+})
+document.getElementById('stop').addEventListener('click', ()=>{
+    document.getElementById('start').style.display = 'inline-flex'
+    document.getElementById('stop').style.display = 'none'
+})
 
 function showChat(){
     let chat = document.createElement('button');
     chat.classList.add('open-button');
     // chat.textContent = 'Chat';
     chat.addEventListener('click', () => showFullChat());
-    chat.addEventListener('click', () => showForm());
+    // chat.addEventListener('click', () => showForm());
 
     let my_div = document.getElementsByClassName("page__body")[0];
     my_div.parentNode.insertBefore(chat, my_div);
@@ -63,13 +84,11 @@ function createFullChat(){
                             <p class="header__title">Ассистент</p>\
                             <button class="header__close" onclick="closeFullChat()">X</button>
                         </div>
-                        <div class="chat__body">
-                            <ul class="body__items" id="messages">
-                            </ul>
+                        <div class="chat__body" id="messages">
                         </div>
                         <div class="chat__input">
                             <div class="input__area">
-                                <input id="txtInput" type="text placeholder="Печатать здесь" autofocus">
+                                <input id="txtInput" type="text" placeholder="Печатать здесь" autofocus">
                             </div>
                             <div id="place_for_btns" class="input__send">
                                 <button id="sendBtn" onclick="sendMessage(event)"><img width="30px" height="30px" src="src/send.svg"></button>
@@ -84,6 +103,8 @@ function createFullChat(){
 function showFullChat(){
     document.getElementById("myForm").style.display = "block";
 }
+
+
     
 
 function closeFullChat(){
@@ -96,8 +117,9 @@ console.log(clientID)
 
 function processMessage(event) {
     var messages = document.getElementById('messages')
-    var message = document.createElement('li')
+    var message = document.createElement('div')
     var content = document.createTextNode(event.data)
+    message.classList.add('items__answers')
     message.appendChild(content);
     messages.appendChild(message);
 }
@@ -105,28 +127,42 @@ function processMessage(event) {
 ws.onmessage = processMessage;
 
 function sendMessage(event) {
+    var messages = document.getElementById('messages')
     var input = document.getElementById("txtInput")
-    var message = document.createElement('li')
+    var message = document.createElement('div')
     var content = document.createTextNode(input.value)
-    message.classList.add('items__item')
+    message.classList.add('items__questions')
     message.appendChild(content);
-    // message.appendChild(message);
+    messages.appendChild(message);
+    console.log("text: " + input.value)
+    document.getElementById('start').style.display = 'inline-flex'
+    document.getElementById('sendBtn').style.display = 'none'
     ws.send(input.value);
+    setTimeout(() => {
+        setScrollPosition();
+      }, 600);
 
     input.value = ''
     event.preventDefault()
 }
 
-function showForm(event) {
-    var button = document.getElementById("connect");
-    var form = document.getElementById("form");
-    console.log(button)
+// function showForm(event) {
     
-    button.style.display = "none";
-    form.style.display = "block";
-}
+//     var form = document.getElementById("form");
+    
+    
+    
+//     form.style.display = "block";
+// }
 
 async function sendVoice(form) {
+    var messages = document.getElementById('messages')
+    let message = document.createElement('div')
+    let img = document.createElement('img')
+    img.classList.add('items__questions_voice')
+    img.setAttribute('src', 'src/mic.svg')
+    message.appendChild(img)
+    messages.appendChild(message)
     let promise = await fetch(URL+"/"+clientID, {
     method: 'POST',
     body: form});
@@ -135,3 +171,14 @@ async function sendVoice(form) {
         console.log(response);
     }
 }
+const setScrollPosition = () => {
+    if (chatBody.scrollHeight > 0) {
+      chatBody.scrollTop = chatBody.scrollHeight;
+    }
+  };
+function ctrlButton() {
+    btn.disabled = this.value.trim().length === 0;
+}
+  
+text1.addEventListener('input', ctrlButton, false);
+ctrlButton.call(text1);
